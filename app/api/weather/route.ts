05e89c38e9d8af3ba0047bb95a457d11d4264e7b
Anay Saha <anaysaha2005@gmail.com>
2025-04-01 22:16:27 +0530

    added genai

--- app/api/weather/route.ts
+++ app/api/weather/route.ts
@@ -1,8 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req:Request){
+import { GoogleGenAI } from "@google/genai";
+export async function GET(req:NextRequest){
 //req.body should contain latitude and longitude
 const {data}=await req.json();
 const latitude=data?.latitude||22;
 const longitude=data?.longitude||73;
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API });
+async function main() {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: "Give soil composition and temperature of lat:"+latitude+"lon:"+longitude+" in json format",
+    });
+    console.log(response.text);
+  }
+  await main();
 }
